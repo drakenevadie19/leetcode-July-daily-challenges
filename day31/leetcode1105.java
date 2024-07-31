849340d3da368c3ee3caf50b4583311b0351dba4
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-31 17:07:36 -0500

    New Change: Add frame for day 31

--- day31/leetcode1105.java
+++ day31/leetcode1105.java
@@ -1,5 +1,7 @@
 package day31;
 
+import java.util.Arrays;
+
 public class leetcode1105 {
     // You are given an array books where books[i] = [thicknessi, heighti] indicates the thickness and height of the ith book. You are also given an integer shelfWidth.
 
@@ -13,6 +15,24 @@ public class leetcode1105 {
     // Return the minimum possible height that the total bookshelf can be after placing shelves in this manner.
 
     public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;  // Base case: no books require 0 height
         
+        for (int i = 1; i <= n; ++i) {
+            int total_width = 0;
+            int max_height = 0;
+            for (int j = i; j > 0; --j) {
+                total_width += books[j-1][0];
+                if (total_width > shelfWidth) {
+                    break;
+                }
+                max_height = Math.max(max_height, books[j-1][1]);
+                dp[i] = Math.min(dp[i], dp[j-1] + max_height);
+            }
+        }
+        
+        return dp[n];
     }
 }
